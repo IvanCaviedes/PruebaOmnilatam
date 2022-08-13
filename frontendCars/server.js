0dc34e1d1f6197dcd91146cd1bdf0cd00724af24
Ivan Caviedes <ivancaviedes99@gmail.com>
2022-08-13 15:46:55 -0500

    arreglando rutas en express

--- frontendCars/server.js
+++ frontendCars/server.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/frontend-cars'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/frontend-cars/index.html'));
-    console.log("este es el dirmname",__dirname)
+app.use(express.static(__dirname + '/dist/frontend-cars'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/frontend-cars/index.html'));
+    console.log("este es el dirmname", __dirname)
 });
-
-app.listen(process.env.PORT || 8080,()=>{
+app.all('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/frontend-cars/index.html'));
+});
+app.listen(process.env.PORT || 8080, () => {
     console.log("servidor corriendo")
 });
