2005f3f1d00906c673874ff9e11edf2bbead1303
Ivan Caviedes <ivancaviedes99@gmail.com>
2022-08-13 12:37:00 -0500

    validando servidor

--- frontendCars/server.js
+++ frontendCars/server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.static(__dirname+'/dist/frontend-cars'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/frontend-cars/index.html'));
+    console.log(__dirname)
 });
 
 app.listen(process.env.PORT || 8080,()=>{
