137a658d90321baefc689ee7aab9dad936a3216e
Ivan Caviedes <ivancaviedes99@gmail.com>
2022-08-13 12:35:55 -0500

    validando servidor

--- frontendCars/server.js
+++ frontendCars/server.js
@@ -8,4 +8,6 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/frontend-cars/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,()=>{
+    console.log("servidor corriendo")
+});
